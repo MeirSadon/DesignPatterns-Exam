01e8c9964190b888f9239c7662f306a0c7dd826d
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2020-03-15 19:51:53 +0200

    Update Root.cs

--- Composite(TreeNumbers) For Test/Composite(TreeNumbers) Fro Test/Root.cs
+++ Composite(TreeNumbers) For Test/Composite(TreeNumbers) Fro Test/Root.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Composite_TreeNumbers__Fro_Test
-{
-    class Root
-    {
-    }
-}
